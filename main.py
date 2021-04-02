5428beb3ed4a34b4ba727539418ef8a359730769
dodiko1d <kirill.anufrienko@gmail.com>
2021-04-02 13:50:22 +0300

    layouts

--- main.py
+++ main.py
@@ -3,12 +3,17 @@ layout_main = []
 
 window = sg.Window('Крестики-нолики', layout=layout_main)
 
+
+counter = 1
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
         break
-    if event == 'button_1':
+    if event == 'button_1' and counter % 2:
         window['button_1'].update(button_color=('#ffffff', '#212121'))
-
+        counter += 1
+    elif event == 'button_1' and not counter % 2:
+        window['button_1'].update(button_color=('#212121', '#ffffff'))
+        counter += 1
 window.close()
 
