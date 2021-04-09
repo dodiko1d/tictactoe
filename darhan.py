11ba6f9b480f047f9a46d9c7e51fc418f8175234
Дархан <zikirindarhan@gmail.com>
2021-04-09 16:18:34 +0700

    debug

---
+++ darhan.py
@@ -0,0 +1,8 @@
+import PySimpleGUIQt
+xod=0
+event=9
+game=['...', '...', '...']
+    for i in range (0,9)
+    list(game[event//3])
+    game[event//3][event%3]='X'*xod%2==1+'O'*xod%2==0
+    str(game[event//3])
