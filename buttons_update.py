ed688e14c4635fe6094accb884067184164c13e3
aurum <knt.ngs..ru>
2021-04-23 17:20:05 +0700

    buttons_update updated

--- buttons_update.py
+++ buttons_update.py
@@ -1,7 +1,10 @@
 import PySimpleGUI as sg
+from board import win
 
 def buttons_update(window: sg.Window,event: str,stats: dict):
     for numb in range(9):
+        button_row=numb//3
+        button_col=numb%3
         if event == f'button_{numb}' and f'button_{numb}' not in stats['used_buttons']:
             if not stats['counter'] % 2:
                 window[f'button_{numb}'].update('X',button_color=('#212121', '#EEFF47'))
@@ -9,3 +12,8 @@ def buttons_update(window: sg.Window,event: str,stats: dict):
                 window[f'button_{numb}'].update('O',button_color=('#212121', '#EEFF47'))
             stats['counter'] += 1
             stats['used_buttons'] |= {f'button_{numb}'}
+            winner = win(stats['field'])
+        if winner == 'D'
+            print('Draw.')
+        else:
+            print(f'Winner is {winner}.')
