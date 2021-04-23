36736f302040abcf83199918978a9db6ab6e21db
aurum <knt.ngs..ru>
2021-04-23 17:27:09 +0700

    buttons_update updated

--- buttons_update.py
+++ buttons_update.py
@@ -8,12 +8,14 @@ def buttons_update(window: sg.Window,event: str,stats: dict):
         if event == f'button_{numb}' and f'button_{numb}' not in stats['used_buttons']:
             if not stats['counter'] % 2:
                 window[f'button_{numb}'].update('X',button_color=('#212121', '#EEFF47'))
+                stats['field'][button_row][button_col] = 'X'
             else:
                 window[f'button_{numb}'].update('O',button_color=('#212121', '#EEFF47'))
+                stats['field'][button_row][button_col] = 'O'
             stats['counter'] += 1
             stats['used_buttons'] |= {f'button_{numb}'}
             winner = win(stats['field'])
-        if winner == 'D'
+        if winner == 'D' and stats['counter'] < 9
             print('Draw.')
         else:
             print(f'Winner is {winner}.')
