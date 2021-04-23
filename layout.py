17be0e8908896264d32e9fe03a3be9b372740825
dodiko1d <kirill.anufrienko@gmail.com>
2021-04-23 13:41:05 +0300

    layouts

--- layout.py
+++ layout.py
@@ -5,4 +5,4 @@ layout = [
     [sg.Button(key=f'button_{x * 3 + z}', size=(30, 9), button_color=('#212121', '#ffffff')) for z in range(3)] for x in range(3)
 ]
 
-layout += [[sg.Text('', key='winner', visible=False)]]
+layout += [[sg.Text('', key='winner', visible=False, font=("Helvetica", 32), size=(50, 3))]]
